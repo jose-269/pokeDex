6e88c19e2bc36c2c9911b249adeb36e17d93df2f
unknown <jlmv84Qgmail.com>
2021-03-07 17:58:27 -0300

    algunos ajustes finales

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,52 +31,48 @@ $("document").ready(function() {
     /***Poke1***/
 
     $("#pokeSelect").on("click", function(y) {
-        
         const pokeSelected = y.target.value;
-        const pokeNormal = pokeSelected.toLowerCase();
+        if(pokeSelected != "0") {
+
         
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon/${pokeNormal}`,
-            type: "GET",
-            dataType: "JSON",
-            success: function(dato) {
-                const pokename = dato.name.toUpperCase();
+            const pokeNormal = pokeSelected.toLowerCase();
+        
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${pokeNormal}`,
+                type: "GET",
+                dataType: "JSON",
+                success: function(dato) {
+                    const pokename = dato.name.toUpperCase();
                 
                 
-                $("#nombreVista").text(pokename)
+                    $("#nombreVista").text(pokename)
                 
-                const pokeFront = dato.sprites.front_default;
-                $("#pokeFoto").attr("src", pokeFront);
+                    const pokeFront = dato.sprites.front_default;
+                    $("#pokeFoto").attr("src", pokeFront);
 
                 // ID y TIPO
 
-                const pokeID = dato.id;
-                $("#id").text(pokeID);
+                    const pokeID = dato.id;
+                    $("#id").text(pokeID);
 
-                const pokeTipo = dato.types[0].type.name;
-                $("#type").text(pokeTipo);
-                console.log(pokeTipo);
-
-               
+                    const pokeTipo = dato.types[0].type.name;
+                    $("#type").text(pokeTipo);
                 
                 // STATS
-                const puntosSalud = dato.stats[0].base_stat;
-                $("#hp").text(puntosSalud);
-                const puntosAtaque = dato.stats[1].base_stat;
-                $("#attack").text(puntosAtaque);
-                const puntosDefensa = dato.stats[2].base_stat;
-                $("#defense").text(puntosDefensa);
-                const ataqueEspecial = dato.stats[3].base_stat;
-                $("#specialAtack").text(ataqueEspecial);
-                const defensaEspecial = dato.stats[4].base_stat;
-                $("#specialDefense").text(defensaEspecial);
-                const velocidad = dato.stats[5].base_stat;
-                $("#speed").text(velocidad);
-
+                    const puntosSalud = dato.stats[0].base_stat;
+                    $("#hp").text(puntosSalud);
+                    const puntosAtaque = dato.stats[1].base_stat;
+                    $("#attack").text(puntosAtaque);
+                    const puntosDefensa = dato.stats[2].base_stat;
+                    $("#defense").text(puntosDefensa);
+                    const ataqueEspecial = dato.stats[3].base_stat;
+                    $("#specialAtack").text(ataqueEspecial);
+                    const defensaEspecial = dato.stats[4].base_stat;
+                    $("#specialDefense").text(defensaEspecial);
+                    const velocidad = dato.stats[5].base_stat;
+                    $("#speed").text(velocidad);
                 
-                
-                
-                const puntosStatsVal1 = [puntosSalud, puntosAtaque, puntosDefensa, ataqueEspecial,
+                    const puntosStatsVal1 = [puntosSalud, puntosAtaque, puntosDefensa, ataqueEspecial,
                     defensaEspecial, velocidad];
                     const pokeStatsName = ["Salud", "Ataque", "Defensa", "Ataque ESP", "Defensa ESP", "Velocidad"];
                     const ctx = document.querySelector("#graph");
@@ -114,10 +110,10 @@ $("document").ready(function() {
                        
                     })
                     
-            }
-        });
-        
-          
+                }
+            });
+        }
+
     });
 
     
@@ -126,51 +122,51 @@ $("document").ready(function() {
     $("#pokeSelect2").on("click", function(z) {
         
         const pokeSelected2 = z.target.value;
-        const pokeNormal2 = pokeSelected2.toLowerCase();
-        $.ajax({
-            url: `https://pokeapi.co/api/v2/pokemon/${pokeNormal2}`,
-            type: "GET",
-            dataType: "JSON",
-            success: function(dato) {
-                const pokename2 = dato.name.toUpperCase();
-                console.log(dato);
-                $("#nombreVista2").text(pokename2)
-                console.log(dato);
-                const pokeFront2 = dato.sprites.front_default;
-                $("#pokeFoto2").attr("src", pokeFront2);
+        if(pokeSelected2 != "0"){
+     
+        
+            const pokeNormal2 = pokeSelected2.toLowerCase();
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${pokeNormal2}`,
+                type: "GET",
+                dataType: "JSON",
+                success: function(dato) {
+                    const pokename2 = dato.name.toUpperCase();
+                    $("#nombreVista2").text(pokename2)
+                    const pokeFront2 = dato.sprites.front_default;
+                    $("#pokeFoto2").attr("src", pokeFront2);
 
                 // ID y TIPO
 
-                const pokeID2 = dato.id;
-                $("#id2").text(pokeID2);
+                    const pokeID2 = dato.id;
+                    $("#id2").text(pokeID2);
 
-                const pokeTipo2 = dato.types[0].type.name;
-                console.log(dato.types);
-                $("#type2").text(pokeTipo2);
+                    const pokeTipo2 = dato.types[0].type.name;
+                    $("#type2").text(pokeTipo2);
 
                 // STATS
-                const puntosSalud2 = dato.stats[0].base_stat;
-                $("#hp2").text(puntosSalud2);
-                const puntosAtaque2 = dato.stats[1].base_stat;
-                $("#attack2").text(puntosAtaque2);
-                const puntosDefensa2 = dato.stats[2].base_stat;
-                $("#defense2").text(puntosDefensa2);
-                const ataqueEspecial2 = dato.stats[3].base_stat;
-                $("#specialAtack2").text(ataqueEspecial2);
-                const defensaEspecial2 = dato.stats[4].base_stat;
-                $("#specialDefense2").text(defensaEspecial2);
-                const velocidad2 = dato.stats[5].base_stat;
-                $("#speed2").text(velocidad2);
+                    const puntosSalud2 = dato.stats[0].base_stat;
+                    $("#hp2").text(puntosSalud2);
+                    const puntosAtaque2 = dato.stats[1].base_stat;
+                    $("#attack2").text(puntosAtaque2);
+                    const puntosDefensa2 = dato.stats[2].base_stat;
+                    $("#defense2").text(puntosDefensa2);
+                    const ataqueEspecial2 = dato.stats[3].base_stat;
+                    $("#specialAtack2").text(ataqueEspecial2);
+                    const defensaEspecial2 = dato.stats[4].base_stat;
+                    $("#specialDefense2").text(defensaEspecial2);
+                    const velocidad2 = dato.stats[5].base_stat;
+                    $("#speed2").text(velocidad2);
 
-                let puntosStatsVal2 = [puntosSalud2, puntosAtaque2, puntosDefensa2, ataqueEspecial2,
+                    let puntosStatsVal2 = [puntosSalud2, puntosAtaque2, puntosDefensa2, ataqueEspecial2,
                     defensaEspecial2, velocidad2];
                  
-                $(".main__pokeGraph-color").addClass("lightGray")
-                $(".main__poke-2").addClass("pokeStile-2")   
-                 const pokeStatsName = ["Salud", "Ataque", "Defensa", "Ataque ESP", "Defensa ESP", "Velocidad"];
-                 const ctx = document.querySelector("#graph");
+                    $(".main__pokeGraph-color").addClass("lightGray")
+                    $(".main__poke-2").addClass("pokeStile-2")   
+                    const pokeStatsName = ["Salud", "Ataque", "Defensa", "Ataque ESP", "Defensa ESP", "Velocidad"];
+                    const ctx = document.querySelector("#graph");
 
-                const graficoPoke = new Chart(ctx, {
+                    const graficoPoke = new Chart(ctx, {
                         type: "horizontalBar",
                         data: {
                             labels: pokeStatsName,
@@ -204,28 +200,9 @@ $("document").ready(function() {
                     
                 }
             });
-            
+        }
             
     });
     
-    /***ChartJS***/
-      
-
-    
-    
-    
    
-
-    // const graficoPoke = new Chart(ctx, {
-    //     type: "radar",
-    //     data: {
-    //         labels: pokeStatsName,
-    //         datasets: [{
-    //             data: puntosStatsVal1,
-    //             label: [pokeNormal],
-    //         }]
-    //     }
-    // })
-
-    
 });
